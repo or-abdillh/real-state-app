6ad1aec099ad6497d69c4213afd3c5c9a73f62e9
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-04-14 17:54:04 +0800

    Create category

---
+++ src/components/Category.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="mt-6 w-full flex justify-between rounded-xl overflow-hidden bg-[#FFFFFF]">
+    <template v-for="(item, x) in category" :key="x">
+      <div
+        @click="current = item"
+        :class="current === item ? 'bg-blue-500 rounded-xl text-gray-50' : ''"
+        class="w-4/12 duration-300 grid place-items-center text-xl py-5 font-medium text-gray-500">
+        {{ item }}
+     </div>
+    </template>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+const category: string[] = [
+  'Buy', 'Rent', 'Sell'
+]
+
+const current: object = ref(category[0])
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
-  <main>
+  <main class="pt-20">
     <HeaderHome />
+    <Category />
   </main>
 </template>
 
 <script setup lang="ts">
 
 import HeaderHome from '@/components/HeaderHome.vue'
+import Category from '@/components/Category.vue' 
 
 </script>
